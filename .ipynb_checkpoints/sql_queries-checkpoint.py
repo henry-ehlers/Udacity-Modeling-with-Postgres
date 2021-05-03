0a1d74fa477d82c1b0cc823bf211278357a2d4fa
Henry Ehlers <henry.ward.ehlers@gmail.com>
2021-05-03 08:11:08 +0000

    basic table creations using only unique keys.

--- .ipynb_checkpoints/sql_queries-checkpoint.py
+++ .ipynb_checkpoints/sql_queries-checkpoint.py
@@ -1,27 +1,43 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+user_table_drop     = "DROP TABLE IF EXISTS users"
+song_table_drop     = "DROP TABLE IF EXISTS songs"
+artist_table_drop   = "DROP TABLE IF EXISTS artists"
+time_table_drop     = "DROP TABLE IF EXISTS times"
 
 # CREATE TABLES
 
 songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS songplays (
+    songplay_id int NOT NULL UNIQUE
+);
 """)
 
 user_table_create = ("""
+CREATE TABLE IF NOT EXISTS users (
+    user_id int NOT NULL UNIQUE
+);
 """)
 
 song_table_create = ("""
+CREATE TABLE IF NOT EXISTS songs (
+    song_id int NOT NULL UNIQUE
+);
 """)
 
 artist_table_create = ("""
+CREATE TABLE IF NOT EXISTS artists (
+    artist_id int NOT NULL UNIQUE
+);
 """)
 
 time_table_create = ("""
+CREATE TABLE IF NOT EXISTS time (
+    start_time numeric NOT NULL
+);
 """)
+
 
 # INSERT RECORDS
 
