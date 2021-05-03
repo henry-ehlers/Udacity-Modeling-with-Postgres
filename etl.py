cc8a681aa2e913fbc497e5ee0ee033dbd2f22fe8
Henry Ehlers <henry.ward.ehlers@gmail.com>
2021-05-03 09:59:14 +0000

    first draft at processing the log data -> time

--- etl.py
+++ etl.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import psycopg2
+from psycopg2 import Error
 import pandas as pd
 from sql_queries import *
 
@@ -9,36 +10,57 @@ def process_song_file(cur, filepath):
     
     # open song file
     # FROM: https://knowledge.udacity.com/questions/459063
-    df = pd.read_json(filepath, lines=True)
+    df = pd.read_json(filepath, lines = True)
 
     # insert song record
     # FROM: https://knowledge.udacity.com/questions/97110
     # FROM: https://zetcode.com/python/psycopg2/
     song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
-    cur.execute(song_table_insert, song_data)
-    
+    # Try insercting the song data
+    try:
+        cur.execute(song_table_insert, song_data)
+    # Ensure the exectution of the insertion was succesful
+    except Error as e:
+        print("Inserction of Song Data Failed - " + e)
+        
     # insert artist record
     # FROM: https://knowledge.udacity.com/questions/97110
     # FROM: https://zetcode.com/python/psycopg2/
     # query = "INSERT INTO cars (id, name, price) VALUES (%s, %s, %s)"
     # cur.executemany(query, cars)
     artist_data = df[['artist_id', 'artist_name', 'artist_location','artist_latitude', 'artist_longitude']].values[0].lolist()
-    cur.execute(artist_table_insert, artist_data)
+    # Try insercting the artist data
+    try:
+        cur.execute(artist_table_insert, artist_data)
+    # Ensure the exectution of the insertion was succesful
+    except Error as e:
+        print("Inserction of Artist Data Failed - " + e)
 
 def process_log_file(cur, filepath):
     # open log file
-    #df = 
+    df = df.read_json(filepath, lines = True) 
 
     # filter by NextSong action
-    #df = 
+    df = df.loc[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
-    #t = 
+    # FROM: https://knowledge.udacity.com/questions/543309
+    t = pd.to_datetime(df['ts'], unit='ms')
     
     # insert time data records
-    #time_data = 
-    #column_labels = 
-    #time_df = 
+    # we have to data files (df and t), which are version of the same file
+    # ONE merely has its contents correctly converted to datetime format
+    time_data = [
+        df.ts.values, 
+        t.dt.hour.values, 
+        t.dt.day.values,
+        t.dt.weekofyear.values, 
+        t.dt.month.values, 
+        t.dt.year.values,
+        t.dt.weekday.values
+    ]
+    column_labels = ["start_time", "hour", "day", "week", "month", "year", "weekday"]
+    time_df = pd.DataFrame(dict(zip(column_labels, time_data)))
 
     for i, row in time_df.iterrows():
         cur.execute(time_table_insert, list(row))
